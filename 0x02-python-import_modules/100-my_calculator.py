a787edfd0462017e8b87eacd627808716a91a71c
ojijoebrain <ojijosephoji@gmail.com>
2023-11-30 17:27:59 -0800

    my_calcualtor

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+
+    operators = ['+', '-', '*', '/']
+
+    if len(argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    elif argv[2] not in operators:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        operand = argv[2]
+
+        if operand == '+':
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif operand == '-':
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+        elif operand == '*':
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+        else:
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
