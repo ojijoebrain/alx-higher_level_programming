82d2d386f3677758da53a49646fe34c8911d3ae1
ojijoebrain <ojijosephoji@gmail.com>
2024-01-09 19:14:27 -0800

    student_11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module 11-student.py """
+
+
+class Student():
+    """ Student class with attributes and a public method
+        to retrive a dict representation of an instance
+    """
+    def __init__(self, first_name, last_name, age):
+        """ initialise the attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrives a dict representation of an instance """
+        if attrs:
+            attr = {}
+            for j in attrs:
+                if j in self.__dict__.keys():
+                    attr[j] = self.__dict__[j]
+            return attr
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """ replaces all attribute of the class """
+        for key, value in json.items():
+            setattr(self, key, value)
