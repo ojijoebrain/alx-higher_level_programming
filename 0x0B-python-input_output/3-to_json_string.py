59a22f18848bbdab6ac45e036ecc1db873cd70ac
ojijoebrain <ojijosephoji@gmail.com>
2024-01-09 18:01:09 -0800

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module 3-to_json_string.py """
+
+import json
+
+
+def to_json_string(my_obj):
+    """ returns JSON representation of a string object """
+    return json.dumps(my_obj)
