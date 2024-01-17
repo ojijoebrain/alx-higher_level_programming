ede08ebdb51aa6523e2e0a2c33b3634115242314
ojijoebrain <ojijoebrain73@gmail.com>
2024-01-17 07:55:49 +0100

    base_update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python3
-""" The base model """
+""" This is the base model """
+
+
+import json
 
 
 class Base():
-    """ The Base class """
+    """ This is the Base class """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ It initializes the id attribute """
+        """ This initializes the id attribute """
         if id is not None:
             self.id = id
         else:
             Base. __nb_objects += 1
-            self.id = Base.__nb_object
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns JSON representation of list_dictionaries """
+        if list_dictionaries is None:
+            return []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes JSON string representation of list_objs """
+        file = cls.__name__ + '.json'
+        with open(file, 'a') as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                for obj in list_objs:
+                    list_dict = [obj.to_dictionary()]
+                f.write(Base.to_json_string(list_dict))
