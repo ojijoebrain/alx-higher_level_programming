71aa994b17b2f08e31b1a75837bf05621083028a
ojijoebrain <ojijosephoji@gmail.com>
2024-02-15 06:04:01 -0800

    task_2_update

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
-tes the database hbtn_0d_2 and the user user_0d_2
+--Creates the database hbtn_0d_2 and the user user_0d_2
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates a user
